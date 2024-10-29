0b8fe36d40810710331fd3b97dc2aa5e1a92e6bf
Ocskó Gábor <ogabor@gmail.com>
2024-10-29 09:01:21 +0100

    schema and hello query

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./data/schema";
 
 const PORT = 8080;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(`Server is running on port ${PORT}`);
 });
 
+const root = {
+  hello: () => "Hello World",
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}/graphql`);
+  console.log(`Server is running on at http://localhost:${PORT}/graphql`);
 });
